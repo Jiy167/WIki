94c303c138261ee660fb61b9b93bec7a8ee25027
JiyuanYang <jiy167@pitt.edu>
2023-08-17 19:36:07 -0400

    integrate hot deployment

--- src/main/java/com/jiyuan/wiki/controller/TestController.java
+++ src/main/java/com/jiyuan/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiyuan.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:test}")
+    private String testHello;
+
 //    @RequestMapping("/hello")
     @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world" + testHello;
     }
     @PostMapping("/hello/post")
     public String hello(String name){
