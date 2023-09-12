b88976dc9a2652d33c30e98662632bf53d949309
JiyuanYang <jiy167@pitt.edu>
2023-09-12 12:39:42 -0400

    make the-header and the-footer components

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Jiyuan ebooks
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header'
+import TheFooter from '@/components/the-footer'
+
+export default defineComponent({
+  name: 'app',
+  components:{
+    TheHeader,
+    TheFooter,
+  }
+});
+</script>
+
 
 <style>
 #components-layout-demo-top-side-2 .logo {
