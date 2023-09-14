c48ce85cd555c2daafca138034b8d2ae7afccc8d
JiyuanYang <jiy167@pitt.edu>
2023-09-14 15:51:52 -0400

    Integrate pagehelper

--- src/main/java/com/jiyuan/wiki/service/EbookService.java
+++ src/main/java/com/jiyuan/wiki/service/EbookService.java
@@ -1,12 +1,16 @@
 package com.jiyuan.wiki.service;
 
 import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiyuan.wiki.domain.Ebook;
 import com.jiyuan.wiki.domain.EbookExample;
 import com.jiyuan.wiki.mapper.EbookMapper;
 import com.jiyuan.wiki.req.EbookReq;
 import com.jiyuan.wiki.resp.EbookResp;
 import com.jiyuan.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -17,17 +21,23 @@ import java.util.List;
 
 @Service
 public class EbookService {
-
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
     public List<EbookResp> list(EbookReq req){
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
 
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("total row：{}", pageInfo.getTotal());
+        LOG.info("total page：{}", pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
