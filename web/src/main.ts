60cccd748f9f397089da3fe52471d3f9a968770d
JiyuanYang <jiy167@pitt.edu>
2023-09-14 11:23:43 -0400

    Vue multi-environment configuration, add dev and prod configuration files Compilation and startup commands add multiple environments

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,3 +6,7 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
 createApp(App).use(router).use(Antd).mount('#app')
+
+
+console.log('environment：', process.env.NODE_ENV);
+console.log('server：', process.env.VUE_APP_SERVER);
