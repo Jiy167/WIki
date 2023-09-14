7abc8771dcbb56c35824e3f7d1acca96dcf1d80b
JiyuanYang <jiy167@pitt.edu>
2023-09-14 12:08:27 -0400

    Add AOP to print interface response time, request and response params. Comment out interceptor.

---
+++ src/main/java/com/jiyuan/wiki/aspect/LogAspect.java
@@ -0,0 +1,94 @@
+package com.jiyuan.wiki.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class LogAspect {
+
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+    /** define a pointcut */
+    @Pointcut("execution(public * com.jiyuan.*.controller..*Controller.*(..))")
+    public void controllerPointcut() {}
+
+//    @Resource
+//    private SnowFlake snowFlake;
+
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+
+//        //
+//        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+
+        // start to print
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+
+        // print request information
+        LOG.info("------------- start -------------");
+        LOG.info("Request address: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("class, function: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("remote address: {}", request.getRemoteAddr());
+
+
+        // print request params
+        Object[] args = joinPoint.getArgs();
+		// LOG.info("request params: {}", JSONObject.toJSONString(args));
+
+		Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // Exclude fields, sensitive fields or fields that are too long are not displayed
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("request params: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // Exclude fields, sensitive fields or fields that are too long are not displayed
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("response results: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- end cost：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+
+}
