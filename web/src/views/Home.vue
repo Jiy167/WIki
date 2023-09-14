dc6804d332662e4a1b9cc0852f94386ab04d5ee0
JiyuanYang <jiy167@pitt.edu>
2023-09-14 11:29:37 -0400

    Change the axios request base URL to read from a configuration file

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -119,8 +119,8 @@ export default defineComponent({
     // const ebooks1 = reactive({books:[]})
 
     onMounted(() =>{
-      console.log("onMounted");
-      axios.get("http://localhost:8880/ebook/list").then((response) => {
+      console.log("onMounted111");
+      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         // ebooks1.books = data.content;
