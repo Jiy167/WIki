a70dccbdc8437ae620ef226731f63e88433691bf
JiyuanYang <jiy167@pitt.edu>
2023-09-14 11:02:34 -0400

    Adjust the layout so that three e-books are displayed on one row

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,24 +48,72 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ebooks}}
-{{ebooks2}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :grid="{ gutter: 16, column: 3 }" :data-source="ebooks">
+
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import { defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from 'axios';
 
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
   name: 'Home',
+  components:{
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
   setup(){
     console.log("setup");
+
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
+
 
     const ebooks = ref();
     // const ebooks1 = reactive({books:[]})
@@ -83,6 +131,9 @@ export default defineComponent({
     return{
       ebooks,
       // ebooks2: toRef(ebooks1, "books"),
+      listData,
+      pagination,
+      actions,
     }
 
   }
