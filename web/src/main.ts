9d7f9f8a560a1eb69444086496dc4e7df70ee293
JiyuanYang <jiy167@pitt.edu>
2023-09-14 11:34:42 -0400

    modify axios' baseURL configuration file

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,9 @@ import router from './router'
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 createApp(App).use(router).use(Antd).mount('#app')
 
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -119,8 +119,8 @@ export default defineComponent({
     // const ebooks1 = reactive({books:[]})
 
     onMounted(() =>{
-      console.log("onMounted111");
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+      console.log("onMounted222");
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         // ebooks1.books = data.content;
