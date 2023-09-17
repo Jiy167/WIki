61e7417f4037d2780a6941caaf4929388aca5f08
JiyuanYang <jiy167@pitt.edu>
2023-09-17 12:46:49 -0400

    Use the tree component to display the document tree. Legacy BUG. The document tree should only check the documents of the current e-book, not all documents.

---
+++ web/src/views/doc.vue
@@ -0,0 +1,75 @@
+<template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <a-row>
+        <a-col :span="6">
+          <a-tree
+              v-if="level1.length > 0"
+              :tree-data="level1"
+              @select="onSelect"
+              :replaceFields="{title: 'name', key: 'id', value: 'id'}"
+              :defaultExpandAll="true"
+          >
+          </a-tree>
+        </a-col>
+        <a-col :span="18">
+        </a-col>
+      </a-row>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref, createVNode } from 'vue';
+import axios from 'axios';
+import {message} from 'ant-design-vue';
+import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
+
+export default defineComponent({
+  name: 'AdminDoc',
+  setup() {
+    const route = useRoute();
+    const docs = ref();
+
+    /**
+     * The first-level classification tree, the children attribute is the second-level classification
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // The first-level classification tree, the children attribute is the second-level classification
+    level1.value = [];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      axios.get("/doc/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          docs.value = data.content;
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    onMounted(() => {
+      handleQuery();
+    });
+
+    return {
+      level1,
+    }
+  }
+});
+</script>
