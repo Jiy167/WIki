c7e32ee50688e922edda70aeb02aeff97b080b30
JiyuanYang <jiy167@pitt.edu>
2023-09-17 11:49:05 -0400

    Document content table design and code generation

--- doc/all2.sql
+++ doc/all2.sql
@@ -77,7 +77,7 @@ insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) val
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (5, 1, 3, 'file2.2', 2, 0, 0);
 insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count) values (6, 1, 5, 'file2.2.1', 1, 0, 0);
 
--- 文档内容
+-- file content
 drop table if exists `content`;
 create table `content` (
   `id` bigint not null comment '文档id',
