36de8473d172bec105ac436a1d91064a79ee31f9
JiyuanYang <jiy167@pitt.edu>
2023-09-20 10:27:55 -0400

    Use vuex to save user information to facilitate other components to obtain login information. Remaining problem: the value is gone after refreshing

---
+++ web/src/store/index.ts
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: {
+    user: {}
+  },
+  mutations: {
+    setUser (state, user) {
+      state.user = user;
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+});
+
+export default store;
