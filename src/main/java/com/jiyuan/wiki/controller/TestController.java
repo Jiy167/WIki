fe7621c5331f5a90711226e01b6536d5b045b847
JiyuanYang <jiy167@pitt.edu>
2023-09-20 09:57:07 -0400

    Add redis test interface

--- src/main/java/com/jiyuan/wiki/controller/TestController.java
+++ src/main/java/com/jiyuan/wiki/controller/TestController.java
@@ -2,17 +2,20 @@ package com.jiyuan.wiki.controller;
 
 import com.jiyuan.wiki.domain.Test;
 import com.jiyuan.wiki.service.TestServive;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @Value("${test.hello:test}")
     private String testHello;
@@ -20,7 +23,10 @@ public class TestController {
     @Resource
     private TestServive testServive;
 
-//    @RequestMapping("/hello")
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    //@RequestMapping("/hello")
     @GetMapping("/hello")
     public String hello(){
         return "hello world" + testHello;
@@ -33,5 +39,20 @@ public class TestController {
     @GetMapping("/test/list")
     public List<Test> list(){
         return testServive.list();
+    }
+
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
